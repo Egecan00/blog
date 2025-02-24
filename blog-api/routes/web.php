dae98898b2d96add7b0dc347e87b68465f1b7ae8
Egecan00 <egecanplt60@gmail.com>
2025-02-24 15:29:52 +0300

    İlk commit

---
+++ blog-api/routes/web.php
@@ -0,0 +1,9 @@
+<?php
+
+use App\Http\Controllers\Auth\LoginController;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\Auth\RegisterController;
+
+
+
