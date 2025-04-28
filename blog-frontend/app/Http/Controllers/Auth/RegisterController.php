0d35fb34ae1448ee84765ea4108c9c4a235b4e2d
Egecan00 <egecanplt60@gmail.com>
2025-04-28 14:52:50 +0300

    güncelff

--- blog-frontend/app/Http/Controllers/Auth/RegisterController.php
+++ blog-frontend/app/Http/Controllers/Auth/RegisterController.php
@@ -4,29 +4,33 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Http; // Laravel HTTP İstemcisi
+use Illuminate\Support\Facades\Http; 
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Http\RedirectResponse;
 
 class RegisterController extends Controller
 {
-    // Kayıt formunu göstermek için bir yöntem
+    
     public function showRegistrationForm()
     {
-        return view('auth.register'); // Kayıt formunun yer aldığı view dosyası
+        return view('auth.register'); 
     }
 
     public function register(Request $request)
     {
-        // Gelen verileri doğrula
+       
         $validator = Validator::make($request->all(), [
             'name' => 'required|string|max:255',
             'email' => 'required|string|email|max:255',
             'password' => 'required|string|min:8|confirmed',
         ]);
 
-     
-       
+        if ($validator->fails()) {
+            return redirect()->back()
+                ->withErrors($validator)
+                ->withInput();
+        }
+        
         $backendResponse = Http::post(env('API_URL').'/register', [
             'name' => $request->name,
             'email' => $request->email,
@@ -34,19 +38,18 @@ class RegisterController extends Controller
             'password_confirmation' => $request->password_confirmation,
         ]);
 
-           
-
+        if ($backendResponse->successful()) 
+        { 
+            return redirect('http://localhost:8001/login')->with('success', 'Kayıt başarılı!'); 
+        } 
+ 
         return redirect()->back()->withErrors([
             'email' => 'Zaten bu E-posta kullanılıyor. Lütfen tekrar deneyin.',
             'password' => 'Şifreniz Şifre Tekrarıyla Uyuşmuyor. Lütfen Tekrar Deneyiniz',
             'password_confirmation' => 'Şifre Tekrarı Şifre İle Uyuşmuyor. Lütfen Tekrar Deneyin',
         ])->withInput();
         
-        if ($backendResponse->successful()) { return redirect('http://localhost:8001/login')->with('success', 'Kayıt başarılı!'); } 
-        
-          if ($validator->fails()) {
-                return response()->json($validator->errors(), 422);
-            }   
+         
        
 
        
